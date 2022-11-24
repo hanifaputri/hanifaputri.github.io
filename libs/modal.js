2bcf48150cf06f6fca0d057475c596f49da24880
Hanifa Putri <rahimahanifa@gmail.com>
2022-11-24 11:00:54 +0700

    add onboarding popup

--- libs/modal.js
+++ libs/modal.js
@@ -1,27 +1,25 @@
-
 /* Modal */
-// Get the modal
-let modal = document.getElementById("myModal");
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+const btnProceed = document.getElementById("btnProceed");
+const onboarding = document.getElementById("onboardingModal");
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  console.log("Button clicked");
+btn.addEventListener("click", () => {
   modal.style.display = "block";
-};
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+btnProceed.addEventListener('click', () => {
+  onboarding.style.display = "none";
+});
+
+span.addEventListener("click", () => {
   modal.style.display = "none";
-}
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.addEventListener("click", (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+});
--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sceneEl = document.querySelector('a-scene');
     // const arSystem = sceneEl.systems["mindar-image-system"];
-
     const video = document.getElementById('ar-video');
-
     const playBtn = document.getElementById('ar-play-btn');
     
     sceneEl.addEventListener("arReady", (event) => {
-        console.log("ar ready");
+        console.log("AR ready");
     });
 
     video.addEventListener('error', () => {
@@ -20,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("target found");
 
         playBtn.addEventListener("click", () => {
-            console.log("Clicked");
+            // console.log("Clicked");
             video.play();
             playBtn.setAttribute('visible', false);
             isClicked = true;
         });
-        console.log(isClicked);
+        // console.log(isClicked);
         if (isClicked){
             video.play();
         }
@@ -34,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
     sceneEl.addEventListener("targetLost", event => {
         video.pause();
         // arSystem.pause(true);
-        console.log("target lost");
+        // console.log("target lost");
     });
     
     sceneEl.addEventListener("arError", (event) => {
